5c312bb081cbbea26fe7cf588afb6137eb3941e8
Anas36 <51910913+Anas36@users.noreply.github.com>
2021-11-09 00:26:52 +0200

    Delete Airline-Reservation-System/app directory

--- Airline-Reservation-System/app/backend/routes/AdminRoutes.js
+++
@@ -1,8 +0,0 @@
-const adminController = require('../Controller/AdminController');
-const router = require('express').Router();
-
-router.route('/add').post(adminController.addAdmin);
-router.route('/').get(adminController.getAllAdmins);
-router.route('/isThereAdmin').get(adminController.getAdmin);
-
-module.exports = router;  
