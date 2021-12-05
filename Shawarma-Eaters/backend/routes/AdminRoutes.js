d54d00e3b38a7a5fe1c5b1beca5997cf40b9db5f
mahmoud99-cell <midonabil852@gmail.com>
2021-12-06 00:48:44 +0200

    .\

--- Shawarma-Eaters/backend/routes/AdminRoutes.js
+++ Shawarma-Eaters/backend/routes/AdminRoutes.js
@@ -4,15 +4,9 @@ const router = require('express').Router();
 
 router.route('/').get(adminController.getAllAdmins);
 router.route('/isThereAdmin').get(adminController.getAdmin);
-<<<<<<< HEAD
 
-=======
->>>>>>> origin/Sprint-2-person-1
 router.route('/add').post(adminController.addAdmin);
 
 
 module.exports = router;  
-<<<<<<< HEAD
 //
-=======
->>>>>>> origin/Sprint-2-person-1
