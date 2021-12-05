ffff3edcd26488e5bd6667e0b607c1d4043d02a1
OmarELmahdy <omar.elmahdy2000@gmail.com>
2021-12-05 16:17:21 +0200

    more seat page changes

--- Shawarma-Eaters/src/components/Seats/seats.component.js
+++ Shawarma-Eaters/src/components/Seats/seats.component.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import axios from 'axios';
+//import CheckIcon from '@mui/icons-material/Check';
+//import ToggleButton from '@mui/material/ToggleButton';
 import "./seats.css";
 
 export default class seats extends Component{
@@ -10,6 +12,7 @@ export default class seats extends Component{
         super(props);
         console.log("inside constructor");
         this.onChangeOcc = this.onChangeOcc.bind(this);  
+        this.toggleOcc = this.toggleOcc.bind(this);  
           this.state = {
             seatnumber:'',
             occupied:false
@@ -22,9 +25,46 @@ export default class seats extends Component{
                 seatnumber:sno.target.value,
                 occupied:true
             })
-            console.log(this.state);
-        }        
+            console.log("Seatnumber: "+this.state.seatnumber);
+            console.log("Occupied: "+this.state.occupied);
+            console.log(""+this.class);
+            console.log(this.className);
+            
+        }   
+        toggleOcc(sno){
+            this.setState({
+                seatnumber:sno.target.value,
+                occupied:true
+             })
+            //  var x = (this.props.occupied===false)?true:false;
+            //  this.setState({occupied:x});
+            if(this.state.occupied){
+                this.setState({
+                    seatnumber:sno.target.value,
+                    occupied:false
+                 })
+                //  this.toggleClass("reserved");
+                document.getElementById("s5").classList.delete("available");
+                document.getElementById("s5").classList.add("reserved");
+            }else{
+                this.setState({
+                    seatnumber:sno.target.value,
+                    occupied:true
+                 })
+                 
+                 document.getElementById("s5").classList.delete("reserved");
+                 document.getElementById("s5").classList.add("available");
+
+                //document.getElementById("s1").className += "reserved";
+                
+
+            }
+             console.log("Seatnumber: "+this.state.seatnumber);
+             console.log("Occupied: "+this.state.occupied);
+}
+
      Seats() {
+        
 //   const [seats,SetSeats]= useState([1,2,3,4,5]);
     }
 componentDidMount() {
@@ -36,8 +76,10 @@ componentDidMount() {
 
 }
 
+
 render(){
 return(
+
 <body>
 
 <h2>Flight Seating</h2>
@@ -50,20 +92,17 @@ return(
 
 </div>
 
-{/* <div class="solid3">Adham
-<h2>Please choose your seat/s</h2>
-
-</div> */}
 
 
 
 
 
+<div class="pbody"> 
+<br></br>
+<br></br>
 
-<h3>Business</h3>
-
-
-<div class="grid-container">
+{/* <h3>Business</h3> */}
+<div class="grid-containerBus">
    
     {/* <div class="item1"> <button class="button" type="button">A1</button></div>
     <div class="item2"> <button class="button" type="button">B1</button></div>
@@ -77,24 +116,28 @@ return(
     <div class="item10"> <button class="button" type="button">E2</button></div>
          */}
         <button id="s1"class="button" onClick={this.econSeat} type="button">1</button>
-        <button id="s2"class="button" onClick={this.onChangeOcc(12)} type="button">2</button>
-        <button id="s3"class="button" type="button">3</button>
+        <button id="s2"class="button" onClick={this.onChangeOcc} type="button">2</button>
+        <button id="s3"class="button" onClick={this.toggleOcc} type="button">3</button>
         <button id="s4"class="button" type="button">4</button>
         <button id="s5"class="button" type="button">5</button>
         <button id="s6"class="button" type="button">6</button>
         <button id="s7"class="button" type="button">7</button>
         <button id="s8"class="button" type="button">8</button> 
         <button id="s9"class="button" type="button">9</button>
-        <button id="s10"class="button" type="button">10</button>
+        <button id="s10"class="reserved" type="button">10</button>
+        <button id="s11"class="reserved" type="button">11</button>
+        <button id="s12"class="reserved" type="button">12</button>
 </div>
-<h3>Economy</h3>
-    <div class="grid-container">
+{/* <h3>Economy</h3> */}
+<br></br>
+    <div class="grid-containerEcon">
+        
    
         <button id="s1"class="button" type="button">1</button>
         <button id="s2"class="button" type="button">2</button>
         <button id="s3"class="button" type="button">3</button>
         <button id="s4"class="button" type="button">4</button>
-        <button id="s5"class="button" type="button">5</button>
+        <button id="s5"class="reserved" type="button">5</button>
         <button id="s6"class="button" type="button">6</button>
         <button id="s7"class="button" type="button">7</button>
         <button id="s8"class="button" type="button">8</button> 
@@ -110,7 +153,7 @@ return(
 
   
   
-
+</div> 
 
 </body>
 
