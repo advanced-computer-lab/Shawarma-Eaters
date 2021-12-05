2731a888219f703aeec49ad429efc787ec6d7648
mahmoud99-cell <midonabil852@gmail.com>
2021-12-06 00:35:50 +0200

    accept change

--- Shawarma-Eaters/backend/routes/AdminRoutes.js
+++ Shawarma-Eaters/backend/routes/AdminRoutes.js
@@ -6,10 +6,5 @@ router.route('/').get(adminController.getAllAdmins);
 router.route('/isThereAdmin').get(adminController.getAdmin);
 router.route('/add').post(adminController.addAdmin);
 
-<<<<<<< HEAD
-module.exports = router;  
-//
-=======
 
 module.exports = router;  
->>>>>>> origin/person2
