ec39b74141d9e34914007cd69e746d555cb50d98
mahmoud99-cell <midonabil852@gmail.com>
2021-12-06 00:23:01 +0200

    Summery

--- Shawarma-Eaters/backend/routes/AdminRoutes.js
+++ Shawarma-Eaters/backend/routes/AdminRoutes.js
@@ -4,7 +4,6 @@ const router = require('express').Router();
 
 router.route('/').get(adminController.getAllAdmins);
 router.route('/isThereAdmin').get(adminController.getAdmin);
-
 router.route('/add').post(adminController.addAdmin);
 
 module.exports = router;  
